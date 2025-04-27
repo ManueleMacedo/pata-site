c70b71fc44dc07acf94e58d65bd3c474140ea0fa
jenixcri <jclc2@cesar.school>
2025-04-27 11:03:10 -0300

    adicionei o botão de voltar para o início

--- src/pages/telalogin.js
+++ src/pages/telalogin.js
@@ -4,14 +4,22 @@ import { Link } from "react-router-dom";
 function TelaLogin() {
   return (
     <div className="tela-login">
-      <div className="login-box">
-        <img src="imagens/logo pref.png" alt="Prefeitura do Recife" className="logo-login" />
-        <h2>Faça login em sua conta ou <a href="#">cadastre-se</a></h2>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
-        <button>Entrar</button>
-        <div className="forgot-password">
-          <Link to="/esqueci-senha">Esqueceu a senha?</Link>
+
+      <div className="login-container">
+      
+        <Link to="/" className="voltar-link">
+          ← Voltar para Início
+        </Link>
+
+        <div className="login-box">
+          <img src="imagens/logo pref.png" alt="Prefeitura do Recife" className="logo-login" />
+          <h2>Faça login em sua conta ou <a href="#">cadastre-se</a></h2>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Senha" />
+          <button>Entrar</button>
+          <div className="forgot-password">
+            <Link to="/esqueci-senha">Esqueceu a senha?</Link>
+          </div>
         </div>
       </div>
     </div>
